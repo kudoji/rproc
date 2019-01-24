b29d9d859cb0cb8ff3125edbfe14dc50f5f92a7c
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-24 17:15:22 +0300

    Application entity created

---
+++ src/main/java/com/heavenhr/rproc/rproc/entities/Application.java
@@ -0,0 +1,83 @@
+/**
+ * @author kudoji
+ */
+package com.heavenhr.rproc.rproc.entities;
+
+import com.heavenhr.rproc.rproc.enums.ApplicationStatus;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Entity
+@Table(
+        name = "applications",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"offer_id", "email"})
+)
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Email(message = "Candidate email is invalid")
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank(message = "Resume cannot be empty")
+    private String resume;
+
+    @NotNull(message = "Application status cannot be empty")
+    @Enumerated
+    private ApplicationStatus applicationStatus;
+
+    @NotNull(message = "Offer must be selected")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Offer offer;
+
+    /**
+     *
+     * @param offer if null removes previous link
+     */
+    public void setOffer(Offer offer){
+        if (offer == null){
+            if (this.offer != null){
+                //  remove previous link
+                log.info("remove application '{}' from the offer '{}'", this, this.offer);
+
+                log.info("before {}", this.offer.getApplications().size());
+                this.offer.getApplications().remove(this);
+                log.info("after {}", this.offer.getApplications().size());
+            }
+
+            this.offer = null;
+        }else{
+            //  in this case nothing to do
+            if (this.offer == offer) return;
+
+            if (this.offer != null){
+                //  remove link to this account from previous currency object
+                this.offer.getApplications().remove(this);
+            }
+
+            this.offer = offer;
+            this.offer.getApplications().add(this);
+        }
+    }
+
+    @PreRemove
+    private void preRemove(){
+        //  destroy bi-directional link to the offer
+        log.info("preRemove() is called for application '{}'", this);
+
+        setOffer(null);
+    }
+}
---
+++ src/main/java/com/heavenhr/rproc/rproc/repositories/ApplicationRepository.java
@@ -0,0 +1,10 @@
+/**
+ * @author kudoji
+ */
+package com.heavenhr.rproc.rproc.repositories;
+
+import com.heavenhr.rproc.rproc.entities.Application;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ApplicationRepository extends CrudRepository<Application, Integer> {
+}
