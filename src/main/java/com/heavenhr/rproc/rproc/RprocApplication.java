9405bd707070713cf356f08a2dd52b9009738813
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-24 14:04:06 +0300

    repo initialized

---
+++ src/main/java/com/heavenhr/rproc/rproc/RprocApplication.java
@@ -0,0 +1,14 @@
+package com.heavenhr.rproc.rproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RprocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RprocApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/heavenhr/rproc/rproc/RprocApplicationTests.java
@@ -0,0 +1,17 @@
+package com.heavenhr.rproc.rproc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RprocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
