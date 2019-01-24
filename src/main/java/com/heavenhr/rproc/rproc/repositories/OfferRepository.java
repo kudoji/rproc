9baf8ab643f868903c6b8a2fe0d1866df6acec78
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-24 16:09:04 +0300

    OfferRepository added

---
+++ src/main/java/com/heavenhr/rproc/rproc/repositories/OfferRepository.java
@@ -0,0 +1,11 @@
+/**
+ * @author kudoji
+ */
+package com.heavenhr.rproc.rproc.repositories;
+
+
+import com.heavenhr.rproc.rproc.entities.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Integer> {
+}
