2dcdcccdf516d5d6315aafd0283d997f299a7c56
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-25 11:54:49 +0300

    get application by offer functionality added

--- src/main/java/com/heavenhr/rproc/rproc/repositories/ApplicationRepository.java
+++ src/main/java/com/heavenhr/rproc/rproc/repositories/ApplicationRepository.java
@@ -4,7 +4,13 @@
 package com.heavenhr.rproc.rproc.repositories;
 
 import com.heavenhr.rproc.rproc.entities.Application;
+import com.heavenhr.rproc.rproc.entities.Offer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ApplicationRepository extends CrudRepository<Application, Integer> {
+    <T> Optional<T> findByIdAndOffer(Integer id, Offer offer);
+
+    <T> Iterable<T> findAllByOffer(Offer offer);
 }
