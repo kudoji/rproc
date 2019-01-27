e223a1d3cda8533c41f6b3cdfc9fb5dc5bc077ac
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-27 14:09:40 +0300

    setting up security (feature #30)

---
+++ src/main/java/com/heavenhr/rproc/rproc/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+/**
+ * @author kudoji
+ */
+package com.heavenhr.rproc.rproc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hr")
+                .password(passwordEncoder().encode("hr"))
+                .roles("hr");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/offers/**").authenticated()
+                .antMatchers("/applications").permitAll()
+                .antMatchers("/", "/**").denyAll()
+            .and()
+                .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
