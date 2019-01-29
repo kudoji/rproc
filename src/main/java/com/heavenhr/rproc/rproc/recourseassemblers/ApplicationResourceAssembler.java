2cf55576ec780b770dc97542f2f90f6d7110c04a
kudoji <36791178+kudoji@users.noreply.github.com>
2019-01-29 19:53:32 +0300

    removed unused import

--- src/main/java/com/heavenhr/rproc/rproc/recourseassemblers/ApplicationResourceAssembler.java
+++ src/main/java/com/heavenhr/rproc/rproc/recourseassemblers/ApplicationResourceAssembler.java
@@ -4,7 +4,6 @@
 package com.heavenhr.rproc.rproc.recourseassemblers;
 
 import com.heavenhr.rproc.rproc.controllers.ApplicationController;
-import com.heavenhr.rproc.rproc.controllers.OfferController;
 import com.heavenhr.rproc.rproc.entities.Application;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
